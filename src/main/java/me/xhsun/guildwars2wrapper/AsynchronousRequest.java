973c3270d1635bebfa213fd7494f2de8ea10d427
xhsun <xhsun@ucalgary.ca>
2017-06-07 20:40:08 -0600

    done all back story asynchronous api

--- src/main/java/me/xhsun/guildwars2wrapper/AsynchronousRequest.java
+++ src/main/java/me/xhsun/guildwars2wrapper/AsynchronousRequest.java
@@ -7,6 +7,8 @@ import me.xhsun.guildwars2wrapper.model.achievements.Achievement;
 import me.xhsun.guildwars2wrapper.model.achievements.AchievementCategory;
 import me.xhsun.guildwars2wrapper.model.achievements.AchievementGroup;
 import me.xhsun.guildwars2wrapper.model.achievements.DailyAchievement;
+import me.xhsun.guildwars2wrapper.model.backstory.BackStoryAnswer;
+import me.xhsun.guildwars2wrapper.model.backstory.BackStoryQuestion;
 import me.xhsun.guildwars2wrapper.model.character.CharacterInventory;
 import me.xhsun.guildwars2wrapper.model.character.Core;
 import me.xhsun.guildwars2wrapper.model.commerce.Prices;
@@ -466,6 +468,64 @@ public class AsynchronousRequest extends Request {
 		isParamValid(new ParamChecker(ids));
 		gw2API.getAchievementGroupInfo(processIds(ids)).enqueue(callback);
 	}
+
+	//Back Story
+
+	/**
+	 * For more info on back story answer API go <a href="https://wiki.guildwars2.com/wiki/API:2/backstory/answers">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see BackStoryAnswer back story answer info
+	 */
+	public void getAllBackStoryAnswerID(Callback<List<String>> callback) throws NullPointerException {
+		gw2API.getAllBackStoryAnswerIDs().enqueue(callback);
+	}
+
+	/**
+	 * For more info on back story answer API go <a href="https://wiki.guildwars2.com/wiki/API:2/backstory/answers">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param ids      list of back story answer id(s)
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws GuildWars2Exception  invalid API key
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see BackStoryAnswer back story answer info
+	 */
+	public void getBackStoryAnswerInfo(String[] ids, Callback<List<BackStoryAnswer>> callback) throws GuildWars2Exception, NullPointerException {
+		isParamValid(new ParamChecker(ids));
+		gw2API.getBackStoryAnswerInfo(processIds(ids)).enqueue(callback);
+	}
+
+	/**
+	 * For more info on back story questions API go <a href="https://wiki.guildwars2.com/wiki/API:2/backstory/questions">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see BackStoryQuestion back story question info
+	 */
+	public void getAllBackStoryQuestionID(Callback<List<Integer>> callback) throws NullPointerException {
+		gw2API.getAllBackStoryQuestionIDs().enqueue(callback);
+	}
+
+	/**
+	 * For more info on back story questions API go <a href="https://wiki.guildwars2.com/wiki/API:2/backstory/questions">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param ids      list of back story question id(s)
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws GuildWars2Exception  invalid API key
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see BackStoryQuestion back story question info
+	 */
+	public void getBackStoryQuestionInfo(int[] ids, Callback<List<BackStoryQuestion>> callback) throws GuildWars2Exception, NullPointerException {
+		isParamValid(new ParamChecker(ids));
+		gw2API.getBackStoryQuestionInfo(processIds(ids)).enqueue(callback);
+	}
+
+
 
 	//Characters
 	/**
