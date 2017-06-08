4782821f3f9eb373e5dbaa02a3fe4c23347955ce
xhsun <xhsun@ucalgary.ca>
2017-06-07 20:41:00 -0600

    done all game build asynchronous api

--- src/main/java/me/xhsun/guildwars2wrapper/AsynchronousRequest.java
+++ src/main/java/me/xhsun/guildwars2wrapper/AsynchronousRequest.java
@@ -525,7 +525,17 @@ public class AsynchronousRequest extends Request {
 		gw2API.getBackStoryQuestionInfo(processIds(ids)).enqueue(callback);
 	}
 
-
+	/**
+	 * For more info on build API go <a href="https://wiki.guildwars2.com/wiki/API:2/build">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see GameBuild game build info
+	 */
+	public void getCurrentGameBuild(Callback<GameBuild> callback) throws NullPointerException {
+		gw2API.getCurrentGameBuild().enqueue(callback);
+	}
 
 	//Characters
 	/**
