4f156c08ac378605a672210f228d96a16b872354
Unknown <cmonesti@gmail.com>
2019-02-28 19:36:46 +0100

    Skills api Async
    Got no one

---
+++ src/main/java/me/xhsun/guildwars2wrapper/requests/v2/skills/Skills.java
@@ -0,0 +1,48 @@
+package me.xhsun.guildwars2wrapper.requests.v2.skills;
+
+import me.xhsun.guildwars2wrapper.GuildWars2;
+import me.xhsun.guildwars2wrapper.error.GuildWars2Exception;
+import me.xhsun.guildwars2wrapper.model.v2.Skill;
+import me.xhsun.guildwars2wrapper.requests.GuildWars2API;
+import me.xhsun.guildwars2wrapper.requests.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
+
+public class Skills extends Request {
+	protected Skills(GuildWars2API gw2API) {
+		super(gw2API);
+	}
+
+	//ASYNC
+	/**
+	 * For more info on Skills API go <a href="https://wiki.guildwars2.com/wiki/API:2/skills">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see Skill skill info
+	 */
+	public void getAllSkillID(Callback<List<Integer>> callback) throws NullPointerException {
+		gw2API.getAllSkillIDs().enqueue(callback);
+	}
+
+	/**
+	 * For more info on Skills API go <a href="https://wiki.guildwars2.com/wiki/API:2/skills">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param ids      list of skill id
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws GuildWars2Exception  empty ID list
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see Skill skill info
+	 */
+	public void getSkillInfo(int[] ids, Callback<List<Skill>> callback) throws GuildWars2Exception, NullPointerException {
+		isParamValid(new ParamChecker(ids));
+		gw2API.getSkillInfo(processIds(ids), GuildWars2.lang.getValue()).enqueue(callback);
+	}
+
+	//SYNC
+}
