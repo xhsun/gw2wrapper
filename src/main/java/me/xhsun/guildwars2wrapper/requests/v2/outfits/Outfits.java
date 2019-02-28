de480540a8effce30e6accfc1d907eb681cef927
Unknown <cmonesti@gmail.com>
2019-02-28 19:35:31 +0100

    Outfits  api Async

---
+++ src/main/java/me/xhsun/guildwars2wrapper/requests/v2/outfits/Outfits.java
@@ -0,0 +1,48 @@
+package me.xhsun.guildwars2wrapper.requests.v2.outfits;
+
+import me.xhsun.guildwars2wrapper.GuildWars2;
+import me.xhsun.guildwars2wrapper.error.GuildWars2Exception;
+import me.xhsun.guildwars2wrapper.model.v2.Outfit;
+import me.xhsun.guildwars2wrapper.requests.GuildWars2API;
+import me.xhsun.guildwars2wrapper.requests.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
+
+public class Outfits extends Request {
+	protected Outfits(GuildWars2API gw2API) {
+		super(gw2API);
+	}
+
+	//ASYNC
+	/**
+	 * For more info on Outfits API go <a href="https://wiki.guildwars2.com/wiki/API:2/outfits">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see Outfit outfit info
+	 */
+	public void getAllOutfitID(Callback<List<Integer>> callback) throws NullPointerException {
+		gw2API.getAllOutfitIDs().enqueue(callback);
+	}
+
+	/**
+	 * For more info on Outfits API go <a href="https://wiki.guildwars2.com/wiki/API:2/outfits">here</a><br/>
+	 * Give user the access to {@link Callback#onResponse(Call, Response)} and {@link Callback#onFailure(Call, Throwable)} methods for custom interactions
+	 *
+	 * @param ids      list of outfit id
+	 * @param callback callback that is going to be used for {@link Call#enqueue(Callback)}
+	 * @throws GuildWars2Exception  empty ID list
+	 * @throws NullPointerException if given {@link Callback} is empty
+	 * @see Outfit outfit info
+	 */
+	public void getOutfitInfo(int[] ids, Callback<List<Outfit>> callback) throws GuildWars2Exception, NullPointerException {
+		isParamValid(new ParamChecker(ids));
+		gw2API.getOutfitInfo(processIds(ids), GuildWars2.lang.getValue()).enqueue(callback);
+	}
+
+	//SYNC
+}
